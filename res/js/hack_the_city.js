c17d7768dac6b65f97a5c42e202c8c73497fc4c4
Brian Chuk <brian_chuk@yahoo.com>
2015-06-21 23:21:01 -0400

    bl

--- res/js/hack_the_city.js
+++ res/js/hack_the_city.js
@@ -37,7 +37,7 @@ function onWindowResize() {
 function render() {
 	requestAnimationFrame( render );
 	cube.rotation.x += 0.01;
-	// cube.rotation.y += 0.1;
+	cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
 render();
