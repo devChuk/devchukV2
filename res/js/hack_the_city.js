0b5dc6331e82f8f3ad8e3a20d11dbe647872c5fe
Brian Chuk <brian_chuk@yahoo.com>
2015-06-26 00:35:59 -0400

    its something, but I really need to make some real progress :disappointed:

--- res/js/hack_the_city.js
+++ res/js/hack_the_city.js
@@ -29,27 +29,41 @@ function onWindowResize() {
 var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 	
-var objects = [];
-var count = 0;
+var objects = {};
+// var count = 0;
 
 // Rendering the scene
 function render() {
-	requestAnimationFrame( render );
-	count++;
-	if (count % 10 == 0) {	
-		var pls = '0x'+Math.floor(Math.random()*16777215).toString(16);
-		var materialz = new THREE.MeshBasicMaterial( { color: 0x6699ff } );
-		var cube = new THREE.Mesh( geometry, materialz );
-		cube.material.color.setHex( pls );
-		scene.add( cube );
-		objects.push(cube);
-	}
+	// requestAnimationFrame( render );
+	// if (count % 10 == 0) {
+	
+		for (var i = -30; i < 31; i++) {
+			var pls = '0x'+Math.floor(Math.random()*16777215).toString(16);
+			var materialz = new THREE.MeshBasicMaterial( { color: 0x6699ff } );
+			var cube = new THREE.Mesh( geometry, materialz );
+			scene.add( cube );
+			cube.position.x += i;
+			cube.material.color.setHex( pls );
+			
 
-	for (var i = 0; i < objects.length; i++) {
-		objects[i].position.z += 0.1;
-		if (objects[i].position.z >= 30)
-			scene.remove(objects[i]);
-	}
+		}
+	// }
+
+	// console.log(count++);
+	// if (count % 10 == 0) {	
+		// var pls = '0x'+Math.floor(Math.random()*16777215).toString(16);
+		// var materialz = new THREE.MeshBasicMaterial( { color: 0x6699ff } );
+		// var cube = new THREE.Mesh( geometry, materialz );
+	// 	cube.material.color.setHex( pls );
+		// scene.add( cube );
+	// 	objects.push(cube);
+	// }
+
+	// for (var i = 0; i < objects.length; i++) {
+	// 	objects[i].position.z += 0.1;
+	// 	if (objects[i].position.z >= 30)
+	// 		scene.remove(objects[i]);
+	// }
 
 
 
