493b273b1bb3c1b425abca5fee2a72e1bf082b94
Brian Chuk <brian_chuk@yahoo.com>
2015-06-27 22:56:36 -0400

    neverending landscape illusion complete. gotta make it pretty now

--- res/js/hack_the_city.js
+++ res/js/hack_the_city.js
@@ -15,7 +15,8 @@ var $container = $('#canvas');
 $container.append(renderer.domElement);
 
 camera.position.z = 30;
-camera.position.y = 2;
+camera.position.y = 5;
+camera.rotation.x = -0.4;
 
 
 // Window resize settings
@@ -30,30 +31,40 @@ var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 	
 var objects = {};
-// var count = 0;
+var count = 0;
+var index = 0;
+// var colors = ['0xFFEBEE', '0xFFCDD2', '0xEF9A9A', '0xE57373', '0xEF5350', '0xF44336', '0xE53935', '0xD32F2F', '0xC62828', '0xB71C1C'];
+var colors = ['0xF44336', '0x9C27B0', '0x2196F3', '0x4CAF50', '0xFFEB3B', '0xFF5722'];
+
 
 // Rendering the scene
 function render() {
-	// requestAnimationFrame( render );
-	// if (count % 10 == 0) {
-	
+	requestAnimationFrame( render );
+	count++;
+	if (count % 10 == 0) {
 		for (var i = -30; i < 31; i++) {
-			var pls = '0x'+Math.floor(Math.random()*16777215).toString(16);
+			var pls = colors[Math.floor(Math.random() * 6)];//'0x'+Math.floor(Math.random()*16777215).toString(16);
 			var materialz = new THREE.MeshBasicMaterial( { color: 0x6699ff } );
 			var cube = new THREE.Mesh( geometry, materialz );
 			scene.add( cube );
 			cube.position.x += i;
 			cube.material.color.setHex( pls );
-			
-
+			objects[index++] = cube;
 		}
-	// }
+	}
+	for (var i in objects) {
+		objects[i].position.z += 0.1;
+		if (objects[i].position.z >= 30) {
+			scene.remove(objects[i]);
+			delete objects[i];
+		}
+	}
 
 	// console.log(count++);
 	// if (count % 10 == 0) {	
 		// var pls = '0x'+Math.floor(Math.random()*16777215).toString(16);
 		// var materialz = new THREE.MeshBasicMaterial( { color: 0x6699ff } );
-		// var cube = new THREE.Mesh( geometry, materialz );
+		// var cube = new THREE.Mesh( geometry, materialz ); 
 	// 	cube.material.color.setHex( pls );
 		// scene.add( cube );
 	// 	objects.push(cube);
