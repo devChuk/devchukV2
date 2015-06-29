8581fb006a8ed9e837e80b7ef084bf15226d0b4b
Brian Chuk <brian_chuk@yahoo.com>
2015-06-29 00:29:49 -0400

    temp. I dunno how I feel about this

--- res/js/hack_the_city.js
+++ res/js/hack_the_city.js
@@ -12,46 +12,99 @@ http://turnislefthome.tumblr.com/post/121867827385/excited-to-have-an-illustrati
 
 */
 
-
-// Creating the scene
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+var count, index, objects, scene, geometry, camera;
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
 
-// Interacting with the HTML
-var $container = $('#canvas');
-$container.append(renderer.domElement);
-
-camera.position.z = 30;
-camera.position.y = 5;
-camera.rotation.x = -0.8;
+var raycaster;
+var mouse = new THREE.Vector2(), INTERSECTED;
 
 
-// Window resize settings
-window.addEventListener( 'resize', onWindowResize, false );
-function onWindowResize() {
-	camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
+
+
+function init() {
+
+	// Creating the scene
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	// Interacting with the HTML
+	var $container = $('#canvas');
+	$container.append(renderer.domElement);
+
+	camera.position.z = 30;
+	camera.position.y = 5;
+	camera.rotation.x = -0.8;
+
+
+	// Window resize settings
+	window.addEventListener( 'resize', onWindowResize, false );
+	function onWindowResize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+	}
+
+	geometry = new THREE.BoxGeometry( 1, 1, 1 );
+
+		
+	objects = {};
+	count = 0;
+	index = 0;
+	// var colors = ['0xFFEBEE', '0xFFCDD2', '0xEF9A9A', '0xE57373', '0xEF5350', '0xF44336', '0xE53935', '0xD32F2F', '0xC62828', '0xB71C1C'];
+	colors = ['0xF44336', '0x9C27B0', '0x2196F3', '0x4CAF50', '0xFFEB3B', '0xFF5722'];
+	var light = new THREE.PointLight( 0xffffff, 1, 0 );
+	light.position.set( 0, 10, 30 );
+	scene.add( light );
+	
+	raycaster = new THREE.Raycaster();
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 }
 
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+function onDocumentMouseMove( event ) {
 
-	
-var objects = {};
-var count = 0;
-var index = 0;
-// var colors = ['0xFFEBEE', '0xFFCDD2', '0xEF9A9A', '0xE57373', '0xEF5350', '0xF44336', '0xE53935', '0xD32F2F', '0xC62828', '0xB71C1C'];
-var colors = ['0xF44336', '0x9C27B0', '0x2196F3', '0x4CAF50', '0xFFEB3B', '0xFF5722'];
-var light = new THREE.PointLight( 0xffffff, 1, 0 );
-light.position.set( 0, 10, 30 );
-scene.add( light );
+	event.preventDefault();
 
+	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+}
+
+function please() {
+
+	var intersects = raycaster.intersectObjects( scene.children );
+
+	if ( intersects.length > 0 ) {
+
+		if ( INTERSECTED != intersects[ 0 ].object ) {
+
+			if ( INTERSECTED ) {
+				INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+			}
+
+			INTERSECTED = intersects[ 0 ].object;
+			INTERSECTED.currentHex = INTERSECTED.material.emissive.getHex();
+			INTERSECTED.material.emissive.setHex( 0xff0000 );
+			INTERSECTED.scale.y += 1;
+		}
+
+	} else {
+
+		if ( INTERSECTED ) INTERSECTED.material.emissive.setHex( INTERSECTED.currentHex );
+
+		INTERSECTED = null;
+
+	}
+
+}
+
+init();
 // Rendering the scene
 function render() {
-	requestAnimationFrame( render );
+	// requestAnimationFrame( render );
+	raycaster.setFromCamera( mouse, camera );
 	count++;
 	if (count % 10 == 0) {
 		for (var i = -30; i < 31; i++) {
@@ -72,24 +125,20 @@ function render() {
 		}
 	}
 
-	// console.log(count++);
-	// if (count % 10 == 0) {	
-		// var pls = '0x'+Math.floor(Math.random()*16777215).toString(16);
-		// var materialz = new THREE.MeshBasicMaterial( { color: 0x6699ff } );
-		// var cube = new THREE.Mesh( geometry, materialz ); 
-	// 	cube.material.color.setHex( pls );
-		// scene.add( cube );
-	// 	objects.push(cube);
-	// }
-
-	// for (var i = 0; i < objects.length; i++) {
-	// 	objects[i].position.z += 0.1;
-	// 	if (objects[i].position.z >= 30)
-	// 		scene.remove(objects[i]);
-	// }
-
+	please();
 
 
 	renderer.render( scene, camera );
 }
 render();
+
+
+function animate() {
+	requestAnimationFrame( animate );
+	render();
+	update();
+
+}
+
+
+animate();
