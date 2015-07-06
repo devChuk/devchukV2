bc1a14bbed86c02bbbcba024c8a70199b79c1ea7
Brian Chuk <brian_chuk@yahoo.com>
2015-07-06 05:09:22 -0400

    contact page. Im pretty sleepy rn

---
+++ res/js/contact.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$( ":button" ).click(function() {
+		// $(this).text(" SENT").addClass("disabled");
+		//https://formden.com/blog/validate-contact-form-jquery
+	});
+});
+	
