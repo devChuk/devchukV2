0ac65c8d5874daf401eb001a23260f118f7a1249
Brian Chuk <brian_chuk@yahoo.com>
2015-07-18 23:46:53 -0400

    automating blog post file creation

--- _posts/new.py
+++ _posts/new.py
@@ -1,15 +1,19 @@
+#Just a simple script to automate the YML frontmatter in new posts
 import datetime
 
 date = raw_input('Creating new file, enter ISO date (if today then just press Enter): ')
 if len(date) == 0:
 	print '\nDate set to: '
 	date = datetime.date.today()
+	date = date.strftime('%Y-%m-%d')
 	print date
+
 title = raw_input('\nEnter title: ')
 
+fileName= date + '-' + title.replace(" ", "_").lower() + '.md'
+print fileName
+file = open(fileName, "w")
 
-
-
-
+file.write('---\nlayout: post\ntitle: ' + title + '\n---\n\n\n\n')
 
 print '\nFile is generated!'
